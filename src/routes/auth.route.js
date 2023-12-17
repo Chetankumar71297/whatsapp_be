578b62e1cc9bde1d40c6298b13c969449edb9449
Chetan kumar <chetankumar71297@gmail.com>
2023-12-17 17:32:46 +0530

    Setup auth middleware for protecting routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,11 +6,15 @@ import {
   refreshToken,
   register,
 } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/register", trimRequest.all, register);
 router.post("/login", trimRequest.all, login);
 router.post("/logout", trimRequest.all, logout);
 router.post("/refreshtoken", trimRequest.all, refreshToken);
+router.get("/testingauthMiddleware", authMiddleware, (req, res) => {
+  res.send(req.user);
+});
 
 export default router;
